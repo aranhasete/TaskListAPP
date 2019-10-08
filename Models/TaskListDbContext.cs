91987ad4320d0aa1ff1391085dd9930ec7c0bd2a
DiegoRLima <aranhajba@gmail.com>
2019-10-07 21:14:57 -0300

    Adicionado DbContext e configurado String de Conexão

---
+++ Models/TaskListDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskListAPP.Models
+{
+    public class TaskListDbContext : DbContext
+    {
+        public TaskListDbContext(DbContextOptions<TaskListDbContext> options) : base(options) { }
+        public DbSet<TaskItem> TaskItems { get; set; }
+    }
+}
