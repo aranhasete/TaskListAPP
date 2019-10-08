758c8f53ea6c3cead85a88e4fc17e4ef37354aeb
DiegoRLima <aranhajba@gmail.com>
2019-10-07 23:50:11 -0300

    Criação do Task Items List no Front-End

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { TaskitemListComponent } from './taskitem-list/taskitem-list.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    TaskitemListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,8 +26,9 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      // { path: 'counter', component: CounterComponent },
+      // { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'taskitem-list', component: TaskitemListComponent },
     ])
   ],
   providers: [],
---
+++ ClientApp/src/app/taskitem-list/taskitem-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-taskitem-list',
+  templateUrl: './taskitem-list.component.html',
+  styleUrls: ['./taskitem-list.component.css']
+})
+export class TaskitemListComponent implements OnInit {
+  public taskItems: TaskItem[];
+  private myHttp: HttpClient;
+  private myBaseUrl: string;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.myHttp = http;
+    this.myBaseUrl = baseUrl;
+    this.getTaskItems();
+  }
+
+  ngOnInit() {
+  }
+
+  getTaskItems() {
+    this.myHttp.get<TaskItem[]>(this.myBaseUrl + 'api/TaskItems').subscribe(result => {
+      this.taskItems = result;
+    }, error => console.error(error));
+  }
+
+}
+
+interface TaskItem {
+  id: number;
+  title: number;
+  isComplete: boolean;
+}
